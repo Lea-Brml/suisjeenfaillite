0ea6915b11e72b477bba24968214382a80550128
Lea-Brml <l.bramoulle@hotmail.fr>
2020-11-15 18:23:00 +0100

    create : edit passif

--- app/controllers/passif_controller.rb
+++ app/controllers/passif_controller.rb
@@ -32,6 +32,24 @@ before_action :authenticate_user!
 
 
   def edit
+    # Méthode qui récupère le potin concerné et l'envoie à la view edit (edit.html.erb) pour affichage dans un formulaire d'édition
+    @passif = Passif.find(params[:id])
+  end
+
+  def update
+    # Méthode qui met à jour le potin à partir du contenu du formulaire de edit.html.erb, soumis par l'utilisateur
+    # pour info, le contenu de ce formulaire sera accessible dans le hash params
+    # Une fois la modification faite, on redirige généralement vers la méthode show (pour afficher le potin modifié)
+
+    @passiff = Passif.find(params[:id])
+
+
+
+        if @passiff.update(salaires: params[:salaires],charges: params[:charges], loyers: params[:loyers], echeances: params[:echeances], dette_fiscale: params[:dette_fiscale], dette_fournisseur: params[:dette_fournisseur])
+          redirect_to root_path
+        else
+          render :edit
+        end
 
 
   end
